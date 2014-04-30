7bd29ca3009b42a2158c31c2637924f992a2675c
inventorbc <inventorbc@gmail.com>
2014-04-30 18:52:10 -0500

    Create GameTest.java

---
+++ GameTest.java
@@ -0,0 +1,8 @@
+import javax.swing.*;
+
+public class GameTest {
+  public static void main(String args[]){
+    GameWindow application = new GameWindow();
+    application.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+  }
+}
