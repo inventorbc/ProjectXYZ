f938d694860be125c0a3d3e206a5d3b34e374b0c
inventorbc <inventorbc@gmail.com>
2014-05-08 11:11:06 -0500

    Delete GameTest.java

--- GameTest.java
+++
@@ -1,8 +0,0 @@
-import javax.swing.*;
-
-public class GameTest {
-  public static void main(String args[]){
-    GameWindow application = new GameWindow();
-    application.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-  }
-}
