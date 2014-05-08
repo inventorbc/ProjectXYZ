68568d9d151d93f9ad9f1f590072b5b75d8771d0
inventorbc <inventorbc@gmail.com>
2014-05-08 11:56:44 -0500

    Create Ground.java

---
+++ Ground.java
@@ -0,0 +1,15 @@
+public class Ground extends GameObject
+{
+   public static final int SIZEX = Display.getWidth();
+   public static final int SIZEY = 20;
+   public Ground()
+   {
+      this.x = x;
+      this.y = y;
+      this.sx = SIZEX;
+      this.sy = SIZEY;
+   }
+   public void update()
+   {
+   }
+}
