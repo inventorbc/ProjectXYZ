568cb0d0098788ed4f98f2ed7a0915ddbabff0fb
inventorbc <inventorbc@gmail.com>
2014-05-08 21:44:54 -0500

    Update GOPlayer.java

--- GOPlayer.java
+++ GOPlayer.java
@@ -3,15 +3,25 @@ import static org.lwjgl.opengl.GL11.*;
 public class GOPlayer extends GameObject{
 	public static final float SIZEX = 30;
 	public static final float SIZEY = 45;
+	private static final int JUMPMAXSPEED = 10;
 	public static float x;
 	public static float y;
+	public float gravityLimit;
+	public float groundLevel;
+	
+	public static boolean isJumping;
+	private boolean climax = false;
 	
 	public Ground ground;
+	private float jumpSpeed;
 	
 	public GOPlayer(float x, float y, Ground ground)
 	{
 		this.x = x;
 		this.y = y;
+		gravityLimit = 250;
+		groundLevel = 240;
+		jumpSpeed = JUMPMAXSPEED;
 		this.ground = ground;
 	}
 	
@@ -21,6 +31,9 @@ public class GOPlayer extends GameObject{
 		{
 			//some stuff
 		}
+		
+		if( isJumping )
+			jump();
 	}
 	
 	public void move(float mag)
@@ -28,9 +41,57 @@ public class GOPlayer extends GameObject{
 		x += mag;
 	}
 	
+	public void jump()
+	{
+		if (isJumping )
+		{
+			
+			y += jumpSpeed;
+			System.out.println("jumpSpeed = " + jumpSpeed);
+			jumpSpeed -= 1;
+			if (jumpSpeed < -JUMPMAXSPEED)
+			{
+				jumpSpeed = JUMPMAXSPEED;
+				isJumping = false;
+			}
+		}
+		
+		/*  Code to be deleted unless needed.
+		 * 
+		 *  For now, we'll keep it.
+		 * 
+		if (y > gravityLimit && climax == false)
+		{
+			climax = true;
+			System.out.println("y > 250, reached climax");
+		}
+		if (y == groundLevel && isJumping) {
+			isJumping = false;
+			climax = false;
+			System.out.println("isJumping = " + isJumping + "climax = " + climax);
+		}
+		else if (climax) {
+			y -= 1;
+			System.out.println("descending");
+		}
+		else if (!climax){
+			y += 1;
+			isJumping = true;
+			System.out.println("still jumping, isJumping = true climax:" + climax);
+		}
+		*/
+	}
+	
+	public void setGravityLimit(float gravity)
+	{
+		gravityLimit = gravity;
+	}
+	
 	public void render()
 	{
 		glColor3f(1f,1f,1f);
 		Draw.rect(x, y, SIZEX, SIZEY);
 	}
+	
+
 }
