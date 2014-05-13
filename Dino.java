736d46fa002455c0ae95cb4678f51984ded17691
inventorbc <inventorbc@gmail.com>
2014-05-12 21:20:20 -0500

    Update Dino.java

--- Dino.java
+++ Dino.java
@@ -4,20 +4,36 @@ import org.lwjgl.opengl.Display;
 
 public class Dino {
    private ArrayList<GameObject> objects;
+   private ArrayList<Ground> groundObjects;
    private GOPlayer player;
-   private GONPC npc;
+   private GONPC npc1;
+   private GONPC npc2;
+   private GONPC npc3;
+   
+   public int groundArraySize = 25;
    
    public Dino()
    {
+	  Display.setTitle("Dino v1.0");
+	  
       objects = new ArrayList<GameObject>();
-      GOSky sky = new GOSky();
-      Ground ground = new Ground(0f, 0f);
-      npc = new GONPC(50, 240, ground);
-      player = new GOPlayer(100,240,ground);
+      groundObjects = new ArrayList<Ground>(groundArraySize);
+      groundObjects.ensureCapacity(25);
+      //npc2 = new GONPC(250, 240, ground);
+      //npc3 = new GONPC(350, 240, ground);
       
-      objects.add(sky);
-      objects.add(ground);
-      objects.add(npc);
+      for(int i = 0; i < groundArraySize; i++)
+      {
+    	Ground ground = new Ground();
+    	groundObjects.add(i, ground);
+    	objects.add(ground);
+      }
+      player = new GOPlayer(Display.getWidth()/2 - GOPlayer.SIZEX/2,
+    		  300-GOPlayer.SIZEY/2, groundObjects);
+      //npc1 = new GONPC(300, 400, groundObjects);
+      //objects.add(npc1);
+      //objects.add(npc2);
+      //objects.add(npc3);
       objects.add(player);
    }
    
@@ -30,9 +46,17 @@ public class Dino {
    public void getInput()
    {
 	   if(Keyboard.isKeyDown(Keyboard.KEY_D) || Keyboard.isKeyDown(Keyboard.KEY_RIGHT))
-		   player.move(2);
+		   player.moveRight(2);
 	   if(Keyboard.isKeyDown(Keyboard.KEY_A) || Keyboard.isKeyDown(Keyboard.KEY_LEFT))
-		   player.move(-2);
+		   player.moveLeft(2);
+	   if(Keyboard.isKeyDown(Keyboard.KEY_SPACE))
+	   {
+		   if(player.canJump)
+		   {
+			   player.isJumping = true;
+ 			   player.jump();
+		   }
+	   }
    }
    
    public void render()
